c055c08b04caeb1a4a5a7a83424b82b701cc868f
Matthew Thor <mjthor@gmail.com>
2017-09-19 10:34:23 -0500

    updated with nunjucks templates etc.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const chalk = require('chalk');
-const app = express();
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
 app.listen(3000, () => {
   console.log(chalk.blue('Listen on :3000'));
@@ -11,8 +16,35 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
   res.send('welcome');
+  next();
 });
 
-app.get('/news',  (req, res, next) => {
+app.get('/news', (req, res, next) => {
   res.send('heers ur news');
+  next();
 });
+
+
+app.get('/views', (req, res, next) => {
+  const locals = {
+    title: 'An Example',
+    people: [
+      { name: 'Gandalf' },
+      { name: 'Frodo' },
+      { name: 'Hermione' },
+      { name: 'Some other fantasy thing' }
+    ],
+  };
+
+  res.render('index', locals, function (err, output) {
+    res.send(output);
+  });
+
+  next();
+});
+
+
+// nunjucks.configure('views', { noCache: true });
+// nunjucks.render('index.html', locals, function (err, output) {
+//   console.log(output);
+// });
